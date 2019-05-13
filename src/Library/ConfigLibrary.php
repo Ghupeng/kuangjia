7fc0e35d575f54396c03d8f9f1c40a38085b8ae6
Ghupeng <ghupeng@163.com>
2019-05-13 18:49:05 +0800

    fix

---
+++ src/Library/ConfigLibrary.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by vim.
+ * User: huguopeng
+ * Date: 2019/05/13
+ * Time: 18:06:39
+ * By: ConfigLibrary.php
+ */
+namespace framing\Library;
+
+class ConfigLibrary {
+	public static function get(){
+		
+	}
+}
